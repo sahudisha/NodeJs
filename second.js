912f2f9bc283f3a748247fd9651769578c4e4991
Disha Sahu <sahu.disha21@gmail.com>
2025-10-14 00:00:50 +0530

    Updated Module Wrapper Function Definition in second.js

--- second.js
+++ second.js
@@ -5,3 +5,10 @@ Arunesh = {
 }
 
 module.exports = Arunesh;
+
+// In Node.js, every module is wrapped in a Module Wrapper Function before execution. This mechanism ensures encapsulation, provides useful variables, and optimizes module handling. The wrapper function looks like this:
+
+// (function (exports, require, module, __filename, __dirname) {
+// // Module code lives here
+// console.log(exports, require, module, __filename, __dirname)
+// });
